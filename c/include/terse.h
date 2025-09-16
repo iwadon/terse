24d73cb0949f2b9432feb34a7abbab3c2d9a6b61
IWATSUKI Hiroyuki <don@na.rim.or.jp>
2025-09-17 03:49:42 +0900

    feat: terse_handle_tを使用してterse_openとterse_close関数を更新し、terse_get_capabilitiesを追加

--- c/include/terse.h
+++ c/include/terse.h
@@ -1,9 +1,22 @@
 #ifndef TERSE_H_INCLUDED
 #define TERSE_H_INCLUDED
 
-typedef struct terse_ctx *terse_ctx_t;
+typedef struct terse_handle *terse_handle_t;
 
-terse_ctx_t terse_open();
-void terse_close(terse_ctx_t ctx);
+typedef enum terse_profile {
+	TERSE_P0 = 0,
+	TERSE_P1,
+	TERSE_P2,
+	TERSE_P3
+} terse_profile_t;
+
+terse_handle_t terse_open(terse_profile_t reqested_profile);
+void terse_close(terse_handle_t handle);
+
+typedef struct terse_capabilities {
+	terse_profile_t profile;
+} terse_capabilities_t;
+
+terse_capabilities_t terse_get_capabilities(terse_handle_t handle);
 
 #endif // TERSE_H_INCLUDED
--- c/src/terse.c
+++ c/src/terse.c
@@ -1,12 +1,20 @@
 #include "terse.h"
 #include <stddef.h>
 
-terse_ctx_t terse_open()
+terse_handle_t terse_open(terse_profile_t reqested_profile)
 {
+	(void)reqested_profile;
 	return NULL;
 }
 
-void terse_close(terse_ctx_t ctx)
+void terse_close(terse_handle_t ctx)
 {
 	(void)ctx;
+}
+
+terse_capabilities_t terse_get_capabilities(terse_handle_t ctx)
+{
+	(void)ctx;
+	terse_capabilities_t caps = { .profile = TERSE_P0 };
+	return caps;
 }
