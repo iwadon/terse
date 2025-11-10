38580001d88705a7c695749dadae01e3be9c42c4
IWATSUKI Hiroyuki <don@na.rim.or.jp>
2025-11-10 23:56:28 +0900

    fix: build-human68kスクリプトにTERSE_ENABLE_ICONVオプションを追加

--- tools/build-human68k.sh
+++ tools/build-human68k.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 project_root=$(dirname "$0")/..
-cmake -S ${project_root} -B ${project_root}/build-human68k -G Ninja -D CMAKE_BUILD_TYPE=Release -D CMAKE_TOOLCHAIN_FILE=./cmake/human68k.cmake
+cmake -S ${project_root} -B ${project_root}/build-human68k -G Ninja -D CMAKE_BUILD_TYPE=Release -D CMAKE_TOOLCHAIN_FILE=./cmake/human68k.cmake -D TERSE_ENABLE_ICONV=OFF
 ninja -C ${project_root}/build-human68k p0_demo
