a20d4652cf5c8c7a8f6655480e21d30893423602
IWATSUKI Hiroyuki <don@na.rim.or.jp>
2025-11-10 19:02:59 +0900

    feat: build-human68kスクリプトを新規作成

---
+++ tools/build-human68k.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -e
+project_root=$(dirname "$0")/..
+cmake -S ${project_root} -B ${project_root}/build-human68k -G Ninja -D CMAKE_BUILD_TYPE=Release -D CMAKE_TOOLCHAIN_FILE=./cmake/human68k.cmake
+ninja -C ${project_root}/build-human68k p0_demo
