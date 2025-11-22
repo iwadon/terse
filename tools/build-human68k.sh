0971f6a0ac68d6737eee20fe09cad1ba654cd133
IWATSUKI Hiroyuki <don@na.rim.or.jp>
2025-11-22 09:11:39 +0900

    fix: build-human68k.shにevent_logger_demo_human68kを追加

--- tools/build-human68k.sh
+++ tools/build-human68k.sh
@@ -2,4 +2,4 @@
 set -e
 project_root=$(dirname "$0")/..
 cmake -S ${project_root} -B ${project_root}/build-human68k -G Ninja -D CMAKE_BUILD_TYPE=Release -D CMAKE_TOOLCHAIN_FILE=./cmake/human68k.cmake
-ninja -C ${project_root}/build-human68k p0_demo p1_color_demo p1_style_demo terse_inspect_terminal terse_profile_info
+ninja -C ${project_root}/build-human68k p0_demo p1_color_demo p1_style_demo terse_inspect_terminal terse_profile_info event_logger_demo_human68k
